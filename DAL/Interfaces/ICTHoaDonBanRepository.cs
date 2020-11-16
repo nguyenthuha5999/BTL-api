ebb78f3e52fd9c13c0328c22da88b14f48947bdf
nguyenthuha5999 <70285724+nguyenthuha5999@users.noreply.github.com>
2020-11-17 00:30:27 +0700

    nguyenthuha5999

---
+++ DAL/Interfaces/ICTHoaDonBanRepository.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Interfaces
+{
+    public interface ICTHoaDonBanRepository
+    {
+        bool Create(CTHoaDonBanModel model);
+        bool Delete(string id);
+        bool Update(CTHoaDonBanModel model);
+
+        CTHoaDonBanModel GetDatabyID(string id);
+        List<CTHoaDonBanModel> GetDataAll();
+
+        List<CTHoaDonBanModel> GetDataSameItem(string Mahdb);
+        List<CTHoaDonBanModel> Search(int pageIndex, int pageSize, out long total, string Mahdb);
+    }
+}
